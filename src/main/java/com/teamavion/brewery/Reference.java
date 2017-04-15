b5f5be9ba6ea09bbc5acd9eebd12e5e884f6c892
TjKenMate <tjkenmate@gmail.com>
2017-04-15 16:21:30 -0400

    AN ARRAY

--- src/main/java/com/teamavion/brewery/Reference.java
+++ src/main/java/com/teamavion/brewery/Reference.java
@@ -1,9 +1,42 @@
 package com.teamavion.brewery;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+
 /**
  * Created by TjKenMate on 4/15/2017
  */
 public class Reference {
 
     public static final String MODID = "brewery", VERSION = "0.1a";
+    public static final Item[] POTION_ITEMS = {
+            Items.SUGAR,
+            Item.getItemFromBlock(Blocks.SOUL_SAND),
+            Items.EMERALD,
+            Item.getItemFromBlock(Blocks.PACKED_ICE),
+            Items.QUARTZ,
+            Items.GOLDEN_APPLE,
+            Items.ARROW,
+            Items.CARROT,
+            Items.NETHER_WART,
+            Items.GOLD_INGOT,
+            Item.getItemFromBlock(Blocks.OBSIDIAN),
+            Items.BLAZE_POWDER,
+            Items.PRISMARINE_SHARD,
+            Items.GHAST_TEAR,
+            Items.COAL,
+            Items.GOLDEN_CARROT,
+            Items.ROTTEN_FLESH,
+            //TEMPORARY ITEM
+            Items.BONE,
+            Items.SPIDER_EYE,
+            Items.SKULL,
+            Items.APPLE,
+            Items.CAKE,
+            Items.GLOWSTONE_DUST,
+            Items.FEATHER,
+            Items.GOLD_NUGGET,
+            Item.getItemFromBlock(Blocks.COBBLESTONE)
+    };
 }
