652baad03c4e3916d8f58a359b737c3f436e2861
TjKenMate <tjkenmate@gmail.com>
2017-04-15 09:18:35 -0400

    Tile Entity Stuff

--- src/main/java/com/teamavion/brewery/block/tile/TileBrewery.java
+++ src/main/java/com/teamavion/brewery/block/tile/TileBrewery.java
@@ -7,8 +7,10 @@ import net.minecraft.util.ITickable;
  * Created by TjKenMate on 4/15/2017XD
  */
 public class TileBrewery extends TileEntity implements ITickable {
-
-
+    private int liquidMB, tempreture, ingredient,
+            ingredient1PotionID, ingredient1Ammount, ingredient1Tempreture, ingredient1Time,
+            ingredient2PotionID, ingredient2Ammount, ingredient2Tempreture, ingredient2Time,
+            ingredient3PotionID, ingredient3Ammount, ingredient3Tempreture, ingredient3Time;
     public TileBrewery(){
 
     }
--- src/main/java/com/teamavion/brewery/recipe/BreweryRecipeHandler.java
+++ src/main/java/com/teamavion/brewery/recipe/BreweryRecipeHandler.java
@@ -10,7 +10,7 @@ public class BreweryRecipeHandler {
     public static boolean isIngredient(Item input){return true;}
     @NotNull
     public static int getPotionId(Item input){return 0;}
-    public static char getPotionGrade(int id, int time, int averageTempreture, int marginOfError,
+    public static char getPotionGrade(int id, int amount, int time, int averageTempreture, int marginOfError,
                                       boolean gradeIncreaseModifier, boolean gradeDecreaseModifer){
         return 'A';}
 }
