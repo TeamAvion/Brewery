9bc2a3ba6ea243167d6cdf00a2753b5a0c7476c6
TjKenMate <tjkenmate@gmail.com>
2017-04-15 12:20:31 -0400

    Seting up wrapper block

---
+++ src/main/java/com/teamavion/brewery/block/BlockBrewery.java
@@ -0,0 +1,27 @@
+package com.teamavion.brewery.block;
+
+import com.teamavion.brewery.block.tile.TileBrewery;
+import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+
+/**
+ * Created by TjKenMate on 4/15/2017.
+ */
+public class BlockBrewery extends Block implements ITileEntityProvider {
+
+    public BlockBrewery(){
+        super(Material.IRON);
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createNewTileEntity(World worldIn, int meta) {
+        return new TileBrewery();
+    }
+
+}
