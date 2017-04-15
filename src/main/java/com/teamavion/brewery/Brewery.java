fe21285a6387f3975344913b06b4fe395bbf6ab1
TjKenMate <tjkenmate@gmail.com>
2017-04-15 08:16:46 -0400

    First Commit

---
+++ src/main/java/com/teamavion/brewery/Brewery.java
@@ -0,0 +1,11 @@
+package com.teamavion.brewery;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Created by TjKenMate on 4/15/2017.
+ */
+
+@Mod(modid = Reference.MODID, version = Reference.VERSION)
+public class Brewery {
+}
---
+++ src/main/java/com/teamavion/brewery/Reference.java
@@ -0,0 +1,9 @@
+package com.teamavion.brewery;
+
+/**
+ * Created by Tjken on 4/15/2017.
+ */
+public class Reference {
+
+    public static final String MODID = "brewery", VERSION = "0.1a";
+}
