2a7b11b41cab7632328401270681057eca4a3c43
TjKenMate <tjkenmate@gmail.com>
2017-04-15 17:13:17 -0400

    Recipe Stuff

--- src/main/java/com/teamavion/brewery/recipe/BreweryRecipeHandler.java
+++ src/main/java/com/teamavion/brewery/recipe/BreweryRecipeHandler.java
@@ -1,15 +1,28 @@
 package com.teamavion.brewery.recipe;
 
 import com.sun.istack.internal.NotNull;
+import com.teamavion.brewery.Reference;
 import net.minecraft.item.Item;
 
 /**
  * Created by TjKenMate on 4/15/2017XD
  */
 public class BreweryRecipeHandler {
-    public static boolean isIngredient(Item input){return true;}
+    public static boolean isIngredient(Item input){
+        for(Item a: Reference.EFFECT_ITEMS)
+            if(a.getUnlocalizedName().equals(input.getUnlocalizedName()))
+                return true;
+        return false;
+    }
     @NotNull
-    public static int getPotionId(Item input){return 0;}
+    public static int getPotionId(Item input) {
+        for (int i = 0; i < Reference.EFFECT_ITEMS.length; i++){
+            if(Reference.EFFECT_ITEMS[i].getUnlocalizedName().equals(input.getUnlocalizedName()))
+                return i;
+        }
+        return 0;
+    }
+
     public static char getPotionGrade(int id, int amount, int time, int averageTempreture, int marginOfError,
                                       boolean gradeIncreaseModifier, boolean gradeDecreaseModifer){
         return 'A';}
