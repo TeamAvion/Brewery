dd2e6fa756abca2ec35c624b26c6a66ef857e37c
TjKenMate <tjkenmate@gmail.com>
2017-04-15 08:49:58 -0400

    First Commit

--- src/main/java/com/teamavion/brewery/Brewery.java
+++ src/main/java/com/teamavion/brewery/Brewery.java
@@ -1,11 +1,30 @@
 package com.teamavion.brewery;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 /**
- * Created by TjKenMate on 4/15/2017.XD
+ * Created by TjKenMate on 4/15/2017
  */
 
 @Mod(modid = Reference.MODID, version = Reference.VERSION)
 public class Brewery {
+
+    @EventHandler
+    public void preInt(FMLPreInitializationEvent e){
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent e){
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent e){
+
+    }
 }
