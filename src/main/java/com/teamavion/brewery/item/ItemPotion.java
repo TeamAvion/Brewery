cdab6d142cf6e4236bc4cb3f8c0131eca9adc261
White Autumn <white@autumn.solutions>
2017-04-15 23:40:43 +0200

    Added some stuff to the item classes

--- src/main/java/com/teamavion/brewery/item/ItemPotion.java
+++ src/main/java/com/teamavion/brewery/item/ItemPotion.java
@@ -1,7 +1,14 @@
 package com.teamavion.brewery.item;
 
+import net.minecraft.item.Item;
+
 /**
  * Created by WhiteAutumn on 2017-04-15.
  */
-public class ItemPotion {
+public class ItemPotion extends Item {
+
+    //TODO: Add durability like flint and steal
+    public ItemPotion() {
+
+    }
 }
--- src/main/java/com/teamavion/brewery/item/ModItems.java
+++ src/main/java/com/teamavion/brewery/item/ModItems.java
@@ -1,7 +1,31 @@
 package com.teamavion.brewery.item;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
 /**
  * Created by WhiteAutumn on 2017-04-15.
  */
 public class ModItems {
+
+    public static Item potion;
+
+    public static void init(){
+        potion = new ItemPotion();
+    }
+
+    public static void register() {
+        GameRegistry.register(potion);
+    }
+
+    public static void registerRenders() {
+        registerRender(potion);
+    }
+
+    private static void registerRender(Item item) {
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0,
+                new ModelResourceLocation(item.getRegistryName(), "inventory"));
+    }
 }
