c37c40def59565a191b3122a5fc29ae993735a6e
White Autumn <white@autumn.solutions>
2017-04-16 18:29:27 +0200

    Changed TileBrewery structure and possible added some errors

--- src/main/java/com/teamavion/brewery/block/tile/TileBrewery.java
+++ src/main/java/com/teamavion/brewery/block/tile/TileBrewery.java
@@ -3,7 +3,6 @@ package com.teamavion.brewery.block.tile;
 import com.teamavion.brewery.item.ModItems;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -12,6 +11,7 @@ import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -23,16 +23,11 @@ import static com.teamavion.brewery.recipe.BreweryRecipeHandler.isIngredient;
  * Created by TjKenMate on 4/15/2017XD
  */
 public class TileBrewery extends TileEntity implements ITickable {
-    private int liquidMB, temperature, ingredient, time,
-            ingredient1PotionID = -100, ingredient1Ammount, ingredient1Tempreture, ingredient1Time = -100,
-            ingredient2PotionID = -100, ingredient2Ammount, ingredient2Tempreture, ingredient2Time = -100,
-            ingredient3PotionID = -100, ingredient3Ammount, ingredient3Tempreture, ingredient3Time = -100;
+    private List<Ingredient> ingredientList = new ArrayList<>(0);
+    private int liquidMB, temperature, ingredientCount, time;
     private boolean temperatureSwitchOn;
 
-    public TileBrewery(){
-        ingredient1Ammount = 0;
-        ingredient2Ammount = 0;
-        ingredient3Ammount = 0;
+    public TileBrewery() {
         temperature = 22;
         liquidMB = 0;
         //For Testing Purposes only
@@ -70,12 +65,10 @@ public class TileBrewery extends TileEntity implements ITickable {
             }
             if((temperature >= 100) && liquidMB > 0)
                 liquidMB--;
-            if(ingredient1Time != -100)
-                ingredient1Time++;
-            if(ingredient2Time != -100)
-                ingredient2Time++;
-            if(ingredient3Time != -100)
-                ingredient3Time++;
+
+            for (Ingredient ingredient: ingredientList) {
+                ingredient.time++;
+            }
             //System.out.println("time: " + time + " temp: " + temperature);
         }
     }
@@ -88,37 +81,17 @@ public class TileBrewery extends TileEntity implements ITickable {
 
        liquidMB = 0;
        temperature = 22;
-       ingredient = 0;
-
-       ingredient1PotionID = -100;
-       ingredient2PotionID = -100;
-       ingredient3PotionID = -100;
-       ingredient1Ammount = 0;
-       ingredient2Ammount = 0;
-       ingredient3Ammount = 0;
-       ingredient1Tempreture = -100;
-       ingredient1Tempreture = -100;
-       ingredient1Tempreture = -100;
-       ingredient1Time = -100;
-       ingredient2Time = -100;
-       ingredient3Time = -100;
+       ingredientCount = 0;
+       ingredientList = new ArrayList<>();
 
        return true;
     }
 
   private NBTTagCompound formPotionNBT(){
         NBTTagCompound compound = new NBTTagCompound();
-        if(ingredient1PotionID != -100){
-            compound.setInteger("potion_ID_1", ingredient1PotionID);
-            compound.setShort("potion_grade_1", getPotionGrade(ingredient1PotionID, ingredient1Ammount, ingredient1Time, ingredient1Tempreture, 5, false, false));
-        }
-        if(ingredient2PotionID != -100){
-            compound.setInteger("potion_ID_2", ingredient2PotionID);
-            compound.setShort("potion_grade_2", getPotionGrade(ingredient2PotionID, ingredient2Ammount, ingredient2Time, ingredient2Tempreture, 5, false, false));
-        }
-        if(ingredient3PotionID != -100){
-            compound.setInteger("potion_ID_3", ingredient3PotionID);
-            compound.setShort("potion_grade_3", getPotionGrade(ingredient3PotionID, ingredient3Ammount, ingredient3Time, ingredient3Tempreture, 5, false, false));
+        for (int i = 0; i < ingredientList.size(); i++) {
+            compound.setInteger("potion_ID_" + i, ingredientList.get(i).id);
+            compound.setShort("potion_grade_" + i, getPotionGrade(ingredientList.get(i).id, ingredientList.get(i).amount, ingredientList.get(i).time, ingredientList.get(i).time,5, false, false));
         }
         return compound;
     }
@@ -152,55 +125,35 @@ public class TileBrewery extends TileEntity implements ITickable {
         return 180;
     }
 
-    public boolean addIngredient(Item input) {
-        if (isIngredient(input)) {
-            if (ingredient < 9) {
-                if (ingredient1PotionID == -100) {
-                    ingredient1PotionID = getPotionId(input);
-                    ingredient1Ammount = 1;
-                    ingredient++;
-                    ingredient1Time = 0;
-                    return   true;
-                } else if (getPotionId(input) == ingredient1PotionID) {
-                    ingredient1Ammount++;
-                    ingredient++;
-                    ingredient1Time = getNewIngredientTime(1);
-                    return true;
-                } else if (ingredient2PotionID == -100) {
-                    ingredient2PotionID = getPotionId(input);
-                    ingredient2Ammount = 1;
-                    ingredient++;
-                    ingredient2Time = 0;
-                    return true;
-                } else if (getPotionId(input) == ingredient2PotionID) {
-                    ingredient2Ammount++;
-                    ingredient++;
-                    ingredient2Time = getNewIngredientTime(2);
-                    return true;
-                } else if (ingredient3PotionID == -100) {
-                    ingredient3PotionID = getPotionId(input);
-                    ingredient3Ammount = 1;
-                    ingredient++;
-                    ingredient3Time = 0;
-                    return true;
-                } else if (getPotionId(input) == ingredient3PotionID) {
-                    ingredient3Ammount++;
-                    ingredient++;
-                    ingredient3Time = getNewIngredientTime(3);
+    public boolean addIngredient(Item item) {
+        if (isIngredient(item) && ingredientCount < 9 && !world.isRemote) {
+            for (Ingredient ingredient : ingredientList) {
+                if (ingredient.id == getPotionId(item)) {
+                    ingredient.amount++;
                     return true;
                 }
             }
+            ingredientList.add(new Ingredient(getPotionId(item), 1, 22, getNewIngredientTime()));
         }
         return false;
     }
 
-    private int getNewIngredientTime(int ingredientNumber){
-        if (ingredientNumber == 1)
-            return 0;
-        if (ingredientNumber == 2)
-            return 0;
-        if (ingredientNumber == 3)
-            return 0;
-        return -256856852;
+    private int getNewIngredientTime(){
+        return 0;
+    }
+
+    private class Ingredient {
+        int id;
+        int amount;
+        int temperature;
+        int time;
+
+        public Ingredient(int id, int amount, int temperature, int time)
+        {
+            this.id = id;
+            this.amount = amount;
+            this.temperature = temperature;
+            this.time = time;
+        }
     }
 }
