58b10e549b141c489f7972ab4e3bc3e1fee84ecc
TjKenMate <tjkenmate@gmail.com>
2017-04-15 23:25:07 -0400

    Catching Null NBTCompound (Still coming up null on creation though)

--- src/main/java/com/teamavion/brewery/item/ItemPotion.java
+++ src/main/java/com/teamavion/brewery/item/ItemPotion.java
@@ -2,19 +2,11 @@ package com.teamavion.brewery.item;
 
 import com.teamavion.brewery.Reference;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
-import net.minecraft.item.EnumAction;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.potion.PotionUtils;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -57,12 +49,14 @@ public class ItemPotion extends ItemFood {
                 addPotion(entityLiving,iPotion3, cPotion3, false);
          */
         if(worldIn.isRemote){return stack;}
-        if(stack.getTagCompound().hasKey("potion_ID_1", 99))
-            addPotion(entityLiving, stack.getTagCompound().getInteger("potion_ID_1"), stack.getTagCompound().getShort("potion_grade_1"), false);
-        if(stack.getTagCompound().hasKey("potion_ID_2", 99))
-            addPotion(entityLiving, stack.getTagCompound().getInteger("potion_ID_2"), stack.getTagCompound().getShort("potion_grade_2"), false);
-        if(stack.getTagCompound().hasKey("potion_ID_3", 99))
-            addPotion(entityLiving, stack.getTagCompound().getInteger("potion_ID_3"), stack.getTagCompound().getShort("potion_grade_3"), false);
+        if(stack.getTagCompound() != null) {
+            if (stack.getTagCompound().hasKey("potion_ID_1", 99))
+                addPotion(entityLiving, stack.getTagCompound().getInteger("potion_ID_1"), stack.getTagCompound().getShort("potion_grade_1"), false);
+            if (stack.getTagCompound().hasKey("potion_ID_2", 99))
+                addPotion(entityLiving, stack.getTagCompound().getInteger("potion_ID_2"), stack.getTagCompound().getShort("potion_grade_2"), false);
+            if (stack.getTagCompound().hasKey("potion_ID_3", 99))
+                addPotion(entityLiving, stack.getTagCompound().getInteger("potion_ID_3"), stack.getTagCompound().getShort("potion_grade_3"), false);
+        }
         stack.damageItem(1, entityLiving);
         if (stack.isEmpty()) {
             return new ItemStack(Items.GLASS_BOTTLE);
