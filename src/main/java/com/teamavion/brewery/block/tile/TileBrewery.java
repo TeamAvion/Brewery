2900feb524c94bd0745622aff5f237ccab5386db
TjKenMate <tjkenmate@gmail.com>
2017-04-17 15:10:45 -0400

    Revert "Fixing potions to actually remvove liquid from the cauldron"
    This reverts commit 5d8a3285ddd70583a6be48e82e2d92d7209b3485.

--- src/main/java/com/teamavion/brewery/block/tile/TileBrewery.java
+++ src/main/java/com/teamavion/brewery/block/tile/TileBrewery.java
@@ -225,13 +225,12 @@ public class TileBrewery extends TileEntity implements ITickable {
 
        liquidMB-=((size+1)*1000);
        temperature = 22;
-       ingredientCount-=tempRemoval;
+       ingredientCount-=ingredientList.size();
        if(liquidMB == 0)
        {
            ingredientList = new ArrayList<>(0);
            ingredientCount = 0;
        }
-       tempRemoval=0;
        sync(this);
 
        return true;
@@ -267,7 +266,6 @@ public class TileBrewery extends TileEntity implements ITickable {
                 ammountHolder = ingredientList.get(i).amount;
             compound.setShort("potion_grade_" + i, getPotionGrade(ingredientList.get(i).id, ammountHolder, ingredientList.get(i).time, ingredientList.get(i).time,1, false, false, liquidMB));
             ingredientList.get(i).amount-=ammountHolder;
-            tempRemoval+=ammountHolder;
             ammountHolder = 0;
         }
         return compound;
