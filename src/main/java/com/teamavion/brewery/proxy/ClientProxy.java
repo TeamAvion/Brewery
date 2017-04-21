c43788673b50fcf29b7c4ef81c67b08fed848024
TjKenMate <tjkenmate@gmail.com>
2017-04-21 00:26:40 -0400

    Rendering stuff

--- src/main/java/com/teamavion/brewery/proxy/ClientProxy.java
+++ src/main/java/com/teamavion/brewery/proxy/ClientProxy.java
@@ -2,7 +2,12 @@ package com.teamavion.brewery.proxy;
 
 
 import com.teamavion.brewery.block.ModBlocks;
+import com.teamavion.brewery.entity.projectile.EntityPotion;
 import com.teamavion.brewery.item.ModItems;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderItem;
+import net.minecraft.client.renderer.entity.RenderSnowball;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 
 @SuppressWarnings("unused")
 public class ClientProxy implements CommonProxy {
@@ -15,6 +20,7 @@ public class ClientProxy implements CommonProxy {
     public void init() {
         ModBlocks.registerRenders();
         ModItems.registerRenders();
+        RenderingRegistry.registerEntityRenderingHandler(EntityPotion.class,new RenderSnowball(Minecraft.getMinecraft().getRenderManager(),ModItems.potionSplash, Minecraft.getMinecraft().getRenderItem()));
     }
 
     @Override
